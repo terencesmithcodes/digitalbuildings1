57e26fffd66a7004fdaa4f35476848a66c2ff0e8
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-10 08:03:00 -0500

    add mongodb and update note model/controller

--- backend/controller/noteController.js
+++ backend/controller/noteController.js
@@ -1,26 +1,57 @@
 //asyncHandler allows us to just use the errorHanlder instead of try catch
 const asyncHandler = require('express-async-handler')
 
+const Note = require('../models/noteModel')
+
 const getNotes = asyncHandler(async(req, res) => {
-  res.status(200).json({message: 'Get Notes'})
+
+  const notes = await Note.find()
+
+  res.status(200).json(notes)
 })
 
 const createNote = asyncHandler(async(req, res) => {
 
   if(!req.body.text){
     res.status(400)
-    throw new Error('Please add a text field')
+    throw new Error('Please add a comment')
   }
 
-  res.status(200).json({message: 'Create Note'})
+  const note = await Note.create({
+    text: req.body.text
+  })
+
+  res.status(200).json(note)
 })
 
 const updateNote = asyncHandler(async(req, res) => {
-  res.status(200).json({message: `Update note ${req.params.id}`})
+
+  const note = await Note.findById(req.params.id)
+
+  if(!note){
+    res.status(400)
+    throw new Error('Note not found')
+  }
+
+  // new option if id does not exist create new note
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  res.status(200).json(updatedNote)
 })
 
 const deleteNote = asyncHandler(async(req, res) => {
-  res.status(200).json({message: `Delete note ${req.params.id}`})
+  const note = await Note.findById(req.params.id)
+
+  if(!note){
+    res.status(400)
+    throw new Error('Note not found')
+  }
+
+  await note.remove()
+
+
+
+  res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
