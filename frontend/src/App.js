95d21e6a8cfc7726d02ee3844a063547897edc55
Terence Smith <terencesmith1@me.com>
2022-09-11 09:51:35 -0400

    added redux, router and toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
