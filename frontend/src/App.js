0ffd54b17af1026dbdfe7e83c68fde8daef7fb7c
Terence Smith <terencesmith1@me.com>
2022-09-13 00:02:11 -0400

    Added login component and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Support from "./components/Support";
 import Footer from "./components/Footer";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
 
 import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <About />
-      <Support />
+      <Routes>
+        
+        <Route index element={<Home />} />
+        <Route path='/Login' element={<Login />} />
+
+      </Routes>
       <Footer />
     </>
   );
