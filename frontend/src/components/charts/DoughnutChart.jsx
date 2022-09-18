43831022d3e2fa33a49eddac6093659e7a98c058
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-18 13:30:54 -0500

    donuts

--- frontend/src/components/charts/DoughnutChart.jsx
+++ frontend/src/components/charts/DoughnutChart.jsx
@@ -6,40 +6,42 @@ import { useSelector, useDispatch } from "react-redux";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 
-const data = {
-  // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  labels:[],
-  datasets: [
-    {
-      label: "# of Votes",
-      // data: [12, 19, 3, 5, 2, 3],
-      data:[],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+
 const DoughnutChart = () => {
+  const data = {
+    // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels:[],
+    datasets: [
+      {
+        label: "# of Votes",
+        // data: [12, 19, 3, 5, 2, 3],
+        data:[],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
    const { equipClasses } =
      useSelector((state) => state.engineer);
-      equipClasses.forEach((equipmentClass) => {
-    data.labels.push(equipmentClass.name)
-    data.datasets[0].data.push(equipmentClass.count)
+
+    equipClasses.forEach((equipmentClass) => {
+      data.labels.push(equipmentClass.name)
+      data.datasets[0].data.push(equipmentClass.count)
     })
       
     return (
