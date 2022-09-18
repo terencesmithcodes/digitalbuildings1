afb3cca9d4bad0611ff6c01a50cb36fbfa7b41f3
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-18 15:43:27 -0500

    refactor building f&b add equiptypes

--- frontend/src/routes/EngineerTest.jsx
+++ frontend/src/routes/EngineerTest.jsx
@@ -7,7 +7,7 @@ import { getEngBuilding, reset } from "../features/engineer/engineerSlice";
 const EngineerTest = () => {
   const dispatch = useDispatch()
 
-  const {building, equipClasses, isError, isSuccess, isLoading, message} =useSelector(
+  const {building, equipClasses, equipTypes, isError, isSuccess, isLoading, message} =useSelector(
     (state) => state.engineer
   )
 
@@ -34,11 +34,20 @@ const EngineerTest = () => {
       <>
       <div>Hello</div>
       <div>{building.Address}</div>
+      <h6>Classes</h6>
       <div>{equipClasses.map((equipmentClass) => (
         <div key={equipmentClass.id}>
           <h4>Class Id: {equipmentClass.id}</h4>
           <h4>Class name: {equipmentClass.name}</h4>
           <h5>Class Num: {equipmentClass.count}</h5>
+        </div>
+      ))}</div>
+      <h6>Types</h6>
+       <div>{equipTypes.map((equipmentTypes) => (
+        <div key={equipmentTypes.id}>
+          <h4>Class Id: {equipmentTypes.id}</h4>
+          <h4>Class name: {equipmentTypes.name}</h4>
+          <h5>Class Num: {equipmentTypes.count}</h5>
         </div>
       ))}</div>
       </>   
