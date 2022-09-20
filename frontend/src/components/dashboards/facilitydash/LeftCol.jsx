7919368a17e186a3658b1ccff5162c62dd99c4e1
Terence Smith <terencesmith1@me.com>
2022-09-20 01:39:40 -0400

    changed font to bold on building info

--- frontend/src/components/dashboards/facilitydash/LeftCol.jsx
+++ frontend/src/components/dashboards/facilitydash/LeftCol.jsx
@@ -46,24 +46,24 @@ const LeftCol = () => {
     return (
       <div className="col-span-3 min-h-[90vh] border-r border-feay-200 items-start justify-start flex flex-col w-full">
         <div className="w-full items-start justify-start flex flex-col px-12 pt-12 pb-6">
-          <h1 className="font-bold text-indigo-600 text-xl xl:text-2xl pb-4">
+          <h1 className="font-bold text-indigo-600 text-xl xl:text-2xl -ml-16 pb-4">
             Building Information
           </h1>
-          <div className="justify-between flex px-6 pt-8 pb-4 border-2 border-feay-200 rounded-xl shadow-xl mt-2 w-full">
+          <div className="justify-between flex px-6 pt-8 pb-4 border-2 border-feay-200 rounded-xl shadow-xl -ml-16 mt-2 w-full">
             <div className="flex flex-col w-full border-r border-feay-200">
-              <span className=" border-b-2 p-2">Building Name:</span>
-              <span className=" border-b-2 p-2">Address: </span>
-              <span className=" border-b-2 p-2">City: </span>
-              <span className=" border-b-2 p-2">State: </span>
-              <span className=" border-b-2 p-2">Zipcode: </span>
-              <span className=" border-b-2 p-2">Phone Number: </span>
-              <span className=" border-b-2 p-2">Type:</span>
-              <span className=" border-b-2 p-2">Area: </span>
-              <span className=" border-b-2 p-2">Floors: </span>
+              <span className=" font-bold border-b-2 p-2">Building Name</span>
+              <span className=" font-bold border-b-2 p-2">Address </span>
+              <span className=" font-bold border-b-2 p-2">City </span>
+              <span className=" font-bold border-b-2 p-2">State </span>
+              <span className=" font-bold border-b-2 p-2">Zipcode </span>
+              <span className=" font-bold border-b-2 p-2">Phone Number </span>
+              <span className=" font-bold border-b-2 p-2">Type</span>
+              <span className=" font-bold border-b-2 p-2">Area </span>
+              <span className=" font-bold border-b-2 p-2">Floors </span>
             </div>
 
             <div className="justify-betweem flex flex-col -px-6  pb-4 w-full">
-              <span className="border-b-2 p-2">{building.BuildingName}</span>
+              <span className=" border-b-2 p-2">{building.BuildingName}</span>
               <span className=" border-b-2 p-2">{building.Address}</span>
               <span className=" border-b-2 p-2">{building.City}</span>
               <span className=" border-b-2 p-2">{building.StateName}</span>
@@ -76,7 +76,7 @@ const LeftCol = () => {
               <span className=" border-b-2 p-2"> {building.Floors}</span>
             </div>
           </div>
-          <div className="w-full items-start justify-start flex flex-col px- pt-12 pb-6">
+          <div className="w-full items-start justify-start flex flex-col -ml-16 pt-12 pb-6">
             <h1 className="font-bold text-indigo-600 text-xl xl:text-2xl pb-4">
               Top Reported Issues
             </h1>
