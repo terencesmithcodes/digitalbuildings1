b68c088cb97aa2f62bf6706ef4873f8ca383c3f5
Terence Smith <terencesmith1@me.com>
2022-09-21 17:52:18 -0400

    added readme and added link to get started button

--- frontend/src/components/landingPage/Hero.jsx
+++ frontend/src/components/landingPage/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import { Link, animateScroll as scroll } from "react-scroll";
 import hero from '../../assets/data-analysis.png'
 
 const Hero = () => {
   return (
-    <div name='home' className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
+    <div
+      name="home"
+      className="w-full h-screen bg-zinc-200 flex flex-col justify-between"
+    >
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
         <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
           <h1 className="py-3 text-5xl md:text-5xl font-bold">
@@ -21,7 +24,15 @@ const Hero = () => {
             <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
             <div className="absolute -bottom-8 left-20 w-72 h-72 bg-orange-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
           </div>
-          <button className="py-3 px-6 sm:w-[60%]">Get Started</button>
+          <Link
+            className="py-3 px-1 sm:w-[60%]"
+            to="support"
+            smooth={true}
+            offset={-80}
+            duration={500}
+          >
+            <button className="py-3 px-16">Get Started</button>
+          </Link>
         </div>
         <div>
           <img className="w-full" src={hero} alt="Hero" />
