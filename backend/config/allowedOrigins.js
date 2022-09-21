d6e846ed490d4cb0b0ec305ad0befcfd2ee287b9
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-21 01:04:22 -0500

    fix deploy backend connection

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://git.heroku.com/digitalbuildingtwoamigos.git'
+  'https://git.heroku.com/digitalbuildingtwoamigos.git',
+  'https://digitalbuildingtwoamigos.herokuapp.com/'
 ]
 
 module.exports = allowedOrigins
