f3b39b6cc0b313eb8dd6021a306bd03c6dbbb926
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-21 00:49:34 -0500

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const colors = require('colors')
@@ -36,6 +37,19 @@ app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/building', require('./routes/engineerRoutes'))
 app.use('/api/energy', require('./routes/energyRoutes'))
 app.use('/api/createbuilding', require('./routes/buildingRoutes'))
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 
 app.use(errorHandler)
