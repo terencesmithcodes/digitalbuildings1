7de5dd78d218a1e30f1cb1d8d322855e83f077a7
Terence Smith <terencesmith1@me.com>
2022-09-21 15:46:23 -0400

    fix line in build select

--- frontend/src/components/dashboards/buildingSelector.jsx/BuildingSelector.jsx
+++ frontend/src/components/dashboards/buildingSelector.jsx/BuildingSelector.jsx
@@ -6,10 +6,10 @@ const BuildingSelector = () => {
   return (
     <div className="w-[1440px] h-screen flex mx-32 my-32 px-10 py-10 ">
       <div className="grid grid-cols-1 md:grid-cols-2 h-[600px] shadow-xl mx-32 my-32  ">
-        <div className="w-[600px] h-[600px] hidden md:block">
+        <div className="w-[600px] h-[600px] border-r border-feay-200 hidden md:block">
           <img className="w-full h-full" src={buildSelectImg} alt="buildimg" />
         </div>
-        <div className="w-[600px] h-[600px] p-4 flex flex-col justify-center border-r border-feay-200">
+        <div className="w-[600px] h-[600px] p-4 flex flex-col justify-center ">
           <form>
             <h2 className="text-2xl font-bold text-indigo-600 text-center mb-8">
               Select Building
